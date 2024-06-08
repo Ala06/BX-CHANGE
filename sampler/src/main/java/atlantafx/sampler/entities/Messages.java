272b2bb13549b5e05a542a99a3f2e11de508c397
Ahmed jemai <84877957+jiimmy69@users.noreply.github.com>
2024-06-08 13:58:50 +0100

    added messages crud

---
+++ sampler/src/main/java/atlantafx/sampler/entities/Messages.java
@@ -0,0 +1,34 @@
+package atlantafx.sampler.entities;
+
+import java.util.Date;
+
+public class Messages {
+
+    private int messageId;
+    private Date date;
+    private String content;
+
+    public int getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(int messageId) {
+        this.messageId = messageId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
